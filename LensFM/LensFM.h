3bf4c7dd4f395a6c9fb0c23bed87114d903a574a
lalson <lalson@hifx.co.in>
2019-09-26 10:26:20 +0530

    Initial Commit

---
+++ LensFM/LensFM.h
@@ -0,0 +1,19 @@
+//
+//  LensFM.h
+//  LensFM
+//
+//  Created by Lalson CL on 26/09/19.
+//  Copyright © 2019 Hifx Media Services Pvt LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LensFM.
+FOUNDATION_EXPORT double LensFMVersionNumber;
+
+//! Project version string for LensFM.
+FOUNDATION_EXPORT const unsigned char LensFMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LensFM/PublicHeader.h>
+
+
